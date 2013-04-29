ce409ff75da196b176a367a1b240f6cbb12e9efc
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2013-04-29 17:54:16 +0200

    Add date and boolean conversions.

---
+++ lib/minimapper/entity/conversions.rb
@@ -0,0 +1,17 @@
+require 'minimapper/entity'
+require "date"
+
+class ToDate
+  def convert(value)
+    Date.parse(value) rescue nil
+  end
+end
+
+class ToBoolean
+  def convert(value)
+    !!value
+  end
+end
+
+Minimapper::Entity::Convert.register_converter(:date, ToDate.new)
+Minimapper::Entity::Convert.register_converter(:boolean, ToBoolean.new)
---
+++ spec/minimapper/entity/conversions_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+require "minimapper/entity/conversions"
+
+class User
+  include Minimapper::Entity
+
+  attribute :signed_up_on, :date
+  attribute :flagged, :boolean
+end
+
+describe "Convertions" do
+  it "can covert dates" do
+    User.new(signed_up_on: "2001-01-01").signed_up_on.should == Date.new(2001, 1, 1)
+    User.new(signed_up_on: "BAD DATA").signed_up_on.should be_nil
+  end
+
+  it "can convert booleans" do
+    User.new(flagged: true).flagged.should eq(true)
+    User.new(flagged: "yep").flagged.should eq(true)
+    User.new(flagged: nil).flagged.should be_nil
+    User.new(flagged: false).flagged.should eq(false)
+  end
+end
