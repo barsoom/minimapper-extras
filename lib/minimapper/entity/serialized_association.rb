b8b00a4021c3fafa21752588fd9b9e148d805dca
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2013-05-13 16:06:20 +0200

    Support creating associated objects using a lambda.

--- lib/minimapper/entity/serialized_association.rb
+++ lib/minimapper/entity/serialized_association.rb
@@ -1,18 +1,26 @@
 require "minimapper/entity"
+require "backports/1.9.1/kernel/public_send"
+require "backports/1.8.7/kernel/instance_exec"
 
 module Minimapper
   module Entity
     module SerializedAssociation
-      def serialized_association(name, entity_class = nil)
-        if entity_class
-          class_name = entity_class.name
-        else
-          class_name = name.to_s.camelcase
-        end
+      def serialized_association(name, entity_class_or_proc = nil)
+        define_method(name) do                                          # def foo
+          attributes = public_send("#{name}_attributes") || {}          #  attributes = foo_attributes
+          entity = instance_variable_get("@#{name}")                    #  entity = @foo
 
-        define_method(name) do                                                # def foo
-          eval("@#{name} ||= ::#{class_name}.new(#{name}_attributes || {})")  #   @foo ||= ::Foo.new(foo_attributes || {})
-        end                                                                   # end
+          if entity_class_or_proc.is_a?(Proc)
+            entity ||= instance_exec(attributes, &entity_class_or_proc) # call proc in instance context
+          else
+            entity_class = entity_class_or_proc ||
+              name.to_s.camelcase.constantize
+
+            entity ||= entity_class.new(attributes)                     # entity ||= Foo.new(attributes)
+          end
+
+          instance_variable_set("@#{name}", entity)                     # @foo = entity
+        end
       end
     end
 
--- spec/minimapper/entity/serialized_association_spec.rb
+++ spec/minimapper/entity/serialized_association_spec.rb
@@ -7,14 +7,32 @@ class Address
   attribute :street
 end
 
+class CustomerAddress
+  include Minimapper::Entity
+
+  def initialize(customer, attributes)
+    @customer = customer
+    super(attributes)
+  end
+
+  def customer_name
+    @customer.name
+  end
+
+  attribute :street
+end
+
 class Customer
   include Minimapper::Entity
 
+  attribute :name
   attribute :address_attributes
   attribute :visit_address_attributes
+  attribute :custom_address_attributes
 
   serialized_association :address
   serialized_association :visit_address, Address
+  serialized_association :custom_address, lambda { |attributes| CustomerAddress.new(self, attributes) }
 end
 
 describe Minimapper::Entity::SerializedAssociation do
@@ -37,5 +55,11 @@ describe Minimapper::Entity::SerializedAssociation do
   it "can use a specified entity type" do
     customer = Customer.new(:visit_address_attributes => { :street => "Street 66" })
     customer.visit_address.street.should == "Street 66"
+  end
+
+  it "can use a lambda to construct addresses" do
+    customer = Customer.new(:name => "Joe", :custom_address_attributes => { :street => "Street 66" })
+    customer.custom_address.street.should == "Street 66"
+    customer.custom_address.customer_name.should == "Joe"
   end
 end
